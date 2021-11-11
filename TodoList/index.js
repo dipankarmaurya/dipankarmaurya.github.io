824baa183f98729d2ff7d66582fe49d3a979def4
Dipankar Maurya <dipankarmaurya5855@gmail.com>
2021-11-11 17:27:21 +0530

    modify todo

--- TodoList/index.js
+++ TodoList/index.js
@@ -10,19 +10,22 @@ addTaskButton.addEventListener("click", function (e) {
   // getting task from input
   let inputTask = document.getElementById("task-name");
   // create new div element and add give it class name task and append it to to do container
-  let div = document.createElement("div");
-  div.classList.add("task");
-  div.innerHTML =
-    '<div class="mark-task">' +
-    "<div class='first'></div>" +
-    "" +
-    "<div>" +
-    inputTask.value +
-    "</div>" +
-    "</div>" +
-        '<div class="delete-task"></div>';
-    inputTask.value = "";
-  todoListConatiner.appendChild(div);
-  //updating task count
-  taskCount.innerHTML = parseInt(taskCount.innerHTML) + 1;
+    if (inputTask.value != "") {
+        let div = document.createElement("div");
+        div.classList.add("task");
+        div.innerHTML =
+        '<div class="mark-task">' +
+        "<div class='first'></div>" +
+        "" +
+        "<div>" +
+        inputTask.value +
+        "</div>" +
+        "</div>" +
+            '<div class="delete-task"></div>';
+        inputTask.value = "";
+      todoListConatiner.appendChild(div);
+      //updating task count
+      taskCount.innerHTML = parseInt(taskCount.innerHTML) + 1; 
+    }
+  
 });
