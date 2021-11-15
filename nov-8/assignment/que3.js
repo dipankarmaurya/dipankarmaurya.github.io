1b5ea4670084552cfd0a3dc4fa652f6f373e2ca9
Dipankar Maurya <dipankarmaurya5855@gmail.com>
2021-11-15 10:31:13 +0530

    solve correctly

--- nov-8/assignment/que3.js
+++ nov-8/assignment/que3.js
@@ -2,18 +2,7 @@
 function paintWall(n, h, w) {
     
     var areaOfWall = h * w;
-    
-    // var count = 0;
-    // while (areaOfWall >= n) {
-    //     areaOfWall -= n;
-    //     count++;
-    // }
-    //or
-
-    if (areaOfWall % n == 0)
-        return areaOfWall / n;
-    
-    return (areaOfWall/n)+1;
+    return n/areaOfWall;
 }
 
 var n = 5;
@@ -22,4 +11,9 @@ var w = 6;
 
 var totalWallPaint = paintWall(n, h, w);
 
-console.log(totalWallPaint);
+if (totalWallPaint > 0) {
+    console.log('total wall painted is --->' + totalWallPaint);
+}
+else {
+    console.log('You need to go shop for buy more paint');
+}
